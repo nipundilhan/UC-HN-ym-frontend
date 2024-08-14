70ff958a872d71b4be68d419d17440138e205510
Hirasha Pooliyadda <hirashapooliyadda20@gmail.com>
2024-08-14 16:34:00 +1200

    created new component for game into

---
+++ src/app/module1_exam_prep/common/module1-enter-game/module1-enter-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-module1-enter-game',
+  templateUrl: './module1-enter-game.component.html',
+  styleUrls: ['./module1-enter-game.component.css']
+})
+export class Module1EnterGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
