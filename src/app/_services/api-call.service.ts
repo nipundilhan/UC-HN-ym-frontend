12b7c9226f1fdcbf6ffa2155ff470b722de4e543
Hirasha Pooliyadda <hirashapooliyadda20@gmail.com>
2025-01-23 22:05:21 +1300

    changes to api services

--- src/app/_services/api-call.service.ts
+++ src/app/_services/api-call.service.ts
@@ -6,7 +6,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class ApiCallService {
 
-  PATH_OF_API = 'http://localhost:3000/YM';
+  PATH_OF_API = 'https://csse-youngmind-ng.canterbury.ac.nz/api/YM';
   requestHeader = new HttpHeaders({ 'No-Auth': 'True' });
 
 
