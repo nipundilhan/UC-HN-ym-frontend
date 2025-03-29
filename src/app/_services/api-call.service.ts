9674660dcab451d13bcc9bf654bd61dc5960b773
Nipun Dilhan <nipundilhanjayarathne@gmail.com>
2025-03-29 16:46:02 +1300

    reverse the api url path

--- src/app/_services/api-call.service.ts
+++ src/app/_services/api-call.service.ts
@@ -6,7 +6,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class ApiCallService {
 
-  PATH_OF_API = 'https://yng-mnd-back-end.onrender.com/YM';
+  PATH_OF_API = 'https://csse-youngmind-ng.canterbury.ac.nz/api/YM';
   requestHeader = new HttpHeaders({ 'No-Auth': 'True' });
 
 
