8db479fcfaf5b3aecd92807b92711c2de06cea38
Nipun Dilhan <nipundilhanjayarathne@gmail.com>
2025-04-18 12:13:20 +1200

    path change reversed

--- src/app/_services/api-call.service.ts
+++ src/app/_services/api-call.service.ts
@@ -8,8 +8,8 @@ export class ApiCallService {
 
 
   
-  //PATH_OF_API = 'https://csse-youngmind.canterbury.ac.nz/api/YM';
-  PATH_OF_API = 'https://yng-mnd-back-end.onrender.com/YM';
+  PATH_OF_API = 'https://csse-youngmind.canterbury.ac.nz/api/YM';
+  //PATH_OF_API = 'https://yng-mnd-back-end.onrender.com/YM';
   
   requestHeader = new HttpHeaders({ 'No-Auth': 'True' });
 
